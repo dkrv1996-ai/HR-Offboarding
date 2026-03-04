1f303c445671d32b63f90f97240ce8d833ff83ce
dkrv1996-ai <dkrv1996@gmail.com>
2026-03-04 11:12:42 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,13 +1,12 @@
 const STORAGE_KEY = "offboardingRequests";
-
 function loadRequests() {
   return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
 }
 function saveRequests(data) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
-
 function generateId() {
   return "REQ-" + Date.now();
 }
+
 
